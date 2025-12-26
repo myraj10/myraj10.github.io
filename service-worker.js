b37a3cb0fe971d462f9cf41ebbd901b1c44682c4
myraj10 <myra.jhawar@gmail.com>
2025-12-26 14:13:32 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,40 @@
-const CACHE_NAME = 'visual-scheduler-cache-v3';
+const CACHE_NAME = 'visual-scheduler-cache-v4';
+
 const urlsToCache = [
-  'index.html',
-  'manifest.json',
-  'assets/sounds/applause.mp3',
-  'icons/icon-192.png',
-  'icons/icon-512.png',
-  // Caching all default task images for offline use
-  'assets/images/defaults/wake_up.png',
-  'assets/images/defaults/dress_up.png',
-  'assets/images/defaults/wash_hands.png',
-  'assets/images/defaults/comb_hair.png',
-  'assets/images/defaults/bus_ride.png',
-  'assets/images/defaults/drawing.png',
-  'assets/images/defaults/ride_bicycle.png',
-  'assets/images/defaults/breakfast.png',
-  'assets/images/defaults/shower.png',
-  'assets/images/defaults/brush_teeth.png',
-  'assets/images/defaults/school.png',
-  'assets/images/defaults/lunch.png',
-  'assets/images/defaults/sports.png',
-  'assets/images/defaults/go_home.png',
-  'assets/images/defaults/parent_pick_up.png',
-  'assets/images/defaults/clean_room.png',
-  'assets/images/defaults/homework.png',
-  'assets/images/defaults/play_games.png',
-  'assets/images/defaults/go_to_sleep.png',
-  'assets/images/defaults/walk_with_a_dog.png',
-  'assets/images/defaults/default.png'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/assets/sounds/applause.mp3',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/assets/images/defaults/wake_up.png',
+  '/assets/images/defaults/dress_up.png',
+  '/assets/images/defaults/wash_hands.png',
+  '/assets/images/defaults/comb_hair.png',
+  '/assets/images/defaults/bus_ride.png',
+  '/assets/images/defaults/drawing.png',
+  '/assets/images/defaults/ride_bicycle.png',
+  '/assets/images/defaults/breakfast.png',
+  '/assets/images/defaults/shower.png',
+  '/assets/images/defaults/brush_teeth.png',
+  '/assets/images/defaults/school.png',
+  '/assets/images/defaults/lunch.png',
+  '/assets/images/defaults/sports.png',
+  '/assets/images/defaults/go_home.png',
+  '/assets/images/defaults/parent_pick_up.png',
+  '/assets/images/defaults/clean_room.png',
+  '/assets/images/defaults/homework.png',
+  '/assets/images/defaults/play_games.png',
+  '/assets/images/defaults/go_to_sleep.png',
+  '/assets/images/defaults/walk_with_a_dog.png',
+  '/assets/images/defaults/default.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Opened cache');
-        // Using addAll which is atomic: if one file fails, the whole operation fails.
+        console.log('Opened root cache');
         return cache.addAll(urlsToCache);
       })
   );
@@ -44,11 +44,10 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Cache hit - return response
         if (response) {
           return response;
         }
-        return fetch(event.request); // Otherwise, fetch from network
+        return fetch(event.request);
       })
   );
 });
@@ -60,7 +59,7 @@ self.addEventListener('activate', event => {
       return Promise.all(
         cacheNames.map(cacheName => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName); // Deleting old caches
+            return caches.delete(cacheName);
           }
         })
       );
